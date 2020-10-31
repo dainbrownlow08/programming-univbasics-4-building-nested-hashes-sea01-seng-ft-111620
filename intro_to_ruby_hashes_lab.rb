9fb6578b86a65f4c893830ab6312c1a642fe3c2f
Dain Brownlow <dain.brownlow@outlook.com>
2020-10-31 00:13:41 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,10 +20,11 @@ end
 
 def monopoly_with_third_tier
   base = {:railroads => {
-   :pieces => 4,
+   pieces: 4,
    :rent_in_dollars => {
-     :one_piece_owned => 25,
+     one_piece_owned: 25,
      two_pieces_owned: 50
+     three_pieces_owned: 100
    },
    :names => {
      
